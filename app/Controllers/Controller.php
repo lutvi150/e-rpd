a253cea08210bf94ebd77d29298eacccb1884901
Rahmat Lutvi Furkon <lutvi1500@gmail.com>
2023-04-02 22:57:45 +0700

    fix laporan

--- app/Controllers/Controller.php
+++ app/Controllers/Controller.php
@@ -4,7 +4,11 @@ namespace App\Controllers;
 
 use App\Controllers\BaseController;
 use App\Models\ModelKegiatan;
+use App\Models\ModelRincianKegiatan;
+use App\Models\ModelRincianKegiatanPerbulan;
+use App\Models\ModelRincianKegiatanPerminggu;
 use App\Models\ModelUnit;
+use App\Models\ModelVerifikasiKegiatan;
 use CodeIgniter\API\ResponseTrait;
 
 class Controller extends BaseController
@@ -13,8 +17,8 @@ class Controller extends BaseController
     public function verifikasi(Type $var = null)
     {
         $session = \Config\Services::session();
-        $verifikasi = new \App\Models\ModelVerifikasiKegiatan;
-        $unit = new \App\Models\ModelUnit;
+        $verifikasi = new ModelVerifikasiKegiatan();
+        $unit = new ModelUnit();
         $id_lembaga = $this->request->getPost('id_lembaga');
         $status = $this->request->getPost('status');
         $comment = $this->request->getPost('comment');
@@ -38,7 +42,7 @@ class Controller extends BaseController
     public function history_verifikasi($id_lembaga)
     {
         $db = \Config\Database::connect();
-        $lembaga = new \App\Models\ModelUnit;
+        $lembaga = new ModelUnit();
         $verifikasi = $db->table('table_verifikasi as a');
         $data['lembaga'] = $lembaga->find($id_lembaga);
         $data['verifikasi'] = $verifikasi->where('a.id_lembaga', $id_lembaga)->join('table_user as b', 'a.created_by=b.id')->select('a.*,b.nama_user')->orderBy('created_at', 'desc')->get()->getResult();
@@ -49,7 +53,7 @@ class Controller extends BaseController
     public function make_report($type, $id_lembaga)
     {
         // use for get data
-        $lembaga = new \App\Models\ModelUnit;
+        $lembaga = new ModelUnit();
         $data['lembaga'] = $lembaga->find($id_lembaga);
         $data['file_name'] = $data['lembaga']->nama_lembaga . "_" . $type;
         if ($type == 'kegiatan') {
@@ -57,7 +61,16 @@ class Controller extends BaseController
             $data['month'] = $this->month('get');
             $data['activity'] = $get_data['activity'];
             $page = view($get_data['page'], $data);
+        } elseif ($type == 'bulanan') {
+            $get_data = $this->report_bulanan($id_lembaga);
+            $data['data_bulanan'] = $get_data;
+            $page = view($get_data['page'], $data);
+        } elseif ($type == 'mingguan') {
+            $get_data = $this->report_mingguan($id_lembaga);
+            $page = view($get_data['page'], $data);
+            $data['data_mingguan'] = $get_data;
         }
+        return $this->respond($data, 200);exit;
         $mpdf = new \Mpdf\Mpdf(['orientation' => 'L']);
         $mpdf->SetTitle($data['file_name']);
         $mpdf->SetAuthor($data['file_name']);
@@ -79,6 +92,90 @@ class Controller extends BaseController
         $kegiatan = new ModelKegiatan();
         $data['activity'] = $kegiatan->asObject()->where('id_lembaga', $id_lembaga)->findAll();
         $data['page'] = 'report/report_kegiatan';
+        return $data;
+    }
+    public function report_bulanan($id_lembaga)
+    {
+        $data['page'] = 'report/report_bulanan';
+        $kegiatan = new ModelKegiatan();
+        $unit = new ModelUnit();
+        $rincian_kegiatan = new ModelRincianKegiatan();
+        $rincian_kegiatan_perbulan = new ModelRincianKegiatanPerbulan();
+        $data_kegiatan = $kegiatan->asObject()->findAll();
+        $array_kegiatan = [];
+        if ($data_kegiatan) {
+            foreach ($data_kegiatan as $key => $value) {
+                $data_rincian_kegiatan = $rincian_kegiatan->asObject()->where('id_kegiatan', $value->id_kegiatan)->findAll();
+                $array_rincian_kegiatan = [];
+                if ($data_rincian_kegiatan) {
+                    foreach ($data_rincian_kegiatan as $key2 => $value2) {
+                        $array_rincian_kegiatan[] = $value2;
+                        $value2->{'rincian_pagu'} = $rincian_kegiatan_perbulan->asObject()->select('total_pagu_perbulan as pagu,bulan')->where('id_rincian_kegiatan', $value2->id_rincian)->orderBy('bulan')->findAll();
+                    }
+                }
+                $array_kegiatan[] = $value;
+                $value->{'detail_kegiatan'} = $array_rincian_kegiatan;
+            }
+        }
+        $data['month'] = $this->month('get');
+        $data['kegiatan'] = $array_kegiatan;
+        return $data;
+    }
+    public function report_mingguan($id_lembaga)
+    {
+        $data['page'] = 'report/report_mingguan';
+        $month = $this->month('convert');
+        $kegiatan = new ModelKegiatan();
+        $unit = new ModelUnit();
+        $rincian_kegiatan = new ModelRincianKegiatan();
+        $rincian_kegiatan_perbulan = new ModelRincianKegiatanPerbulan();
+        $rincian_kegiatan_perminggu = new ModelRincianKegiatanPerminggu();
+        $data_kegiatan = $kegiatan->asObject()->findAll();
+        $array_kegiatan = [];
+        // $new_data_month = [];
+        $array_mingguan = [];
+        if ($data_kegiatan) {
+            foreach ($data_kegiatan as $key => $value) {
+                $data_rincian_kegiatan = $rincian_kegiatan->asObject()->where('id_kegiatan', $value->id_kegiatan)->findAll();
+                if ($data_rincian_kegiatan) {
+                    $value->{'rincian_kegiatan'} = $data_rincian_kegiatan;
+                } else {
+                    $value->{'rincian_kegiatan'} = [];
+                }
+                $array_kegiatan[] = $value;
+            }
+        }
+        foreach ($month as $key_month => $value_month) {
+            $array_mingguan[] = $value_month;
+            // if ($data_kegiatan) {
+            //     foreach ($data_kegiatan as $key => $value) {
+            //         $data_rincian_kegiatan = $rincian_kegiatan->asObject()->where('id_kegiatan', $value->id_kegiatan)->findAll();
+            //         $array_rincian_kegiatan = [];
+            //         if ($data_rincian_kegiatan) {
+            //             foreach ($data_rincian_kegiatan as $key2 => $value2) {
+            //                 $array_rincian_kegiatan[] = $value2;
+            //                 $hitung_rincian = $rincian_kegiatan_perbulan->asObject()->select('total_pagu_perbulan,id_rincian_kegiatan_perbulan')->where('bulan', $key)->where('id_rincian_kegiatan', $value2->id_rincian)->first();
+            //                 $value2->{'harga_satuan'} = $hitung_rincian == null ? 0 : $hitung_rincian->total_pagu_perbulan;
+            //                 if ($hitung_rincian) {
+            //                     $value2->{'rincian_perminggu'} = $rincian_kegiatan_perminggu->where('id_rincian_kegiatan_perbulan', $hitung_rincian->id_rincian_kegiatan_perbulan)->orderBy('minggu', 'asc')->findAll();
+            //                 } else {
+            //                     $value2->{'rincian_perminggu'} = $hitung_rincian;
+            //                 }
+            //             }
+            //             $array_kegiatan[] = $value;
+            //             $value->{'detail_kegiatan'} = $array_rincian_kegiatan;
+            //         }
+            //     }
+            //     $value_month->{'number_month'} = $key_month;
+            //     $value_month->{'kegiatan'} = $array_kegiatan;
+            //     $new_data_month[] = $value_month;
+            // }
+
+        }
+        $data['kegiatan'] = $array_kegiatan;
+        $data['mingguan'] = $array_mingguan;
+        // $data['month'] = $month;
+        // $data['new_month'] = $new_data_month;
         return $data;
     }
     // costume bulan
---
+++ app/Views/report/report_bulanan.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <style>
+        body {
+            font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+            /* font-size: 12px; */
+        }
+
+        table,
+        th,
+        td {
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+
+        .no-border-table tr td {
+            border: 0px solid rgb(255, 255, 255) !important;
+            height: 20px !important;
+        }
+
+        table tr td {
+            height: 40px;
+        }
+
+        .head td {
+            text-align: center;
+            height: 50px;
+        }
+        .head-table td{
+            text-align: center;
+        }
+    </style>
+    <table class="no-border-table" style="width: 100%;text-align: center;font-weight: bold;">
+        <tr>
+            <td>Tabel 2.2</td>
+        </tr>
+        <tr>
+            <td>Rencana Penarikan Dana Bulanan</td>
+        </tr>
+        <tr>
+            <td><?=$lembaga->nama_lembaga?></td>
+        </tr>
+        <tr>
+            <td>Tahun Anggaran <?=date('Y')?></td>
+        </tr>
+    </table>
+    <table style="width: 100%;" >
+        <tr>
+            <td colspan="3">Kementrian Negara/ Lembaga</td>
+            <td colspan="14">: Kementrian Agama</td>
+        </tr>
+        <tr>
+            <td colspan="3">Satuan Kerja</td>
+            <td colspan="14">: IAIN Batusangkar</td>
+        </tr>
+        <tr>
+            <td colspan="3">Fak/PPs/Lembaga/Unit</td>
+            <td colspan="14">: <?=$lembaga->nama_lembaga?></td>
+        </tr>
+        <tr class="head-table" style="background-color: rgb(115, 120, 116);text-align: center;">
+            <td>Kode</td>
+            <td>Uraian</td>
+            <td>Pagu (Rp)</td>
+            <?php foreach ($data_bulanan['month'] as $key => $value): ?>
+            <td style="width:80px"><?=$value['month']?></td>
+            <?php endforeach;?>
+            <td>Jumlah</td>
+        </tr>
+        <?php foreach ($data_bulanan['kegiatan'] as $key => $kegiatan): ?>
+        <tr>
+            <td style="font-weight: bold;" ><?=$kegiatan->kode_kegiatan?></td>
+            <td style="font-weight: bold;" ><?=$kegiatan->uraian_kegiatan?></td>
+            <td>Rp.<?=number_format($kegiatan->pagu_kegiatan)?></td>
+            <?php for ($i = 0; $i < 12; $i++): ?>
+            <td></td>
+            <?php endfor;?>
+            <td colspan="2"></td>
+        </tr>
+        <?php $total = 0;foreach ($kegiatan->detail_kegiatan as $key => $detail_kegiatan): ?>
+        <tr>
+            <td style="text-align: right;"><?=$detail_kegiatan->kode_rincian?></td>
+            <td><?=$detail_kegiatan->uraian_rincian_kegiatan?></td>
+            <td>Rp.<?=number_format($detail_kegiatan->pagu_rincian_kegiatan)?></td>
+            <?php for ($i = 0; $i < 12; $i++): ?>
+                <?php $pagu = 0;if (isset($detail_kegiatan->rincian_pagu)) {foreach ($detail_kegiatan->rincian_pagu as $key => $rincian_pagu) {
+    if ($i == (int) $rincian_pagu->bulan) {
+        $pagu = $rincian_pagu->pagu;
+        break;
+    }
+}
+    $total += $pagu;
+}?>
+            <td  style="text-align: right;"><?=number_format($pagu)?></td>
+            <?php endfor;?>
+            <td colspan="2"><?=number_format($total)?></td>
+        </tr>
+        <?php $total = 0;endforeach;?>
+        <?php endforeach;?>
+        <!-- use for data  -->
+
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td style="text-align: center;" data-bulan="<?=$i?>">
+                <span class=""></span>
+            </td>
+
+        </tr>
+        <!-- end data  -->
+    </table>
+</body>
+
+</html>
